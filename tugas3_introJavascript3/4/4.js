5b23cdde4675e219a69d07846b987ebb423e0b59
dicky08 <dickyf147@gmail.com>
2020-08-17 07:38:00 +0700

    testing

--- tugas3_introJavascript3/4/4.js
+++ tugas3_introJavascript3/4/4.js
@@ -1,7 +1,13 @@
 // Fetch API Ambil data Nama
-const fetch = require('node-fetch');
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(result => {
-        result.map(names => console.log(names.name));
+
+const fetch = require("node-fetch");
+const expres = require("express");
+const aps = expres();
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then((result) => {
+        result.map((names) => console.log(names.name));
     });
+
+
+aps.listen(3000);
