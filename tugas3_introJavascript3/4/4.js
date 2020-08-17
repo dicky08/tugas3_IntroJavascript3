2a97e116b444b35242eb332b3128dc756fcae241
dicky08 <dickyf147@gmail.com>
2020-08-17 19:01:27 +0700

    edit Console

--- tugas3_introJavascript3/4/4.js
+++ tugas3_introJavascript3/4/4.js
@@ -3,11 +3,10 @@
 const fetch = require("node-fetch");
 const expres = require("express");
 const aps = expres();
+
 fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res) => res.json())
-    .then((result) => {
-        result.map((names) => console.log(names.name));
-    });
-
-
+  .then((res) => res.json())
+  .then((result) => {
+    result.map((names) => console.log(names.name));
+  });
 aps.listen(3000);
